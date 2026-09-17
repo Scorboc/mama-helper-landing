d20a96997a9bf6698ced11bc37a5bea045d049b2
Scorboc <63611346+Scorboc@users.noreply.github.com>
2026-09-17 21:21:58 +0300

    Refine transparent glass surfaces in both themes

--- src/main.tsx
+++ src/main.tsx
@@ -2,5 +2,6 @@ import { createRoot } from 'react-dom/client'
 import App from './App'
 import './index.css'
 import './palette-lock.css'
+import './glass-theme.css'
 
 createRoot(document.getElementById("root")!).render(<App />);
